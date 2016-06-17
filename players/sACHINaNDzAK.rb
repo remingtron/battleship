ab8569f401ce9747c07aaed7c7769de243507bc4
POET <POET>
2016-06-17 15:49:43 -0400

    Zak and sachin stuff

--- players/sACHINaNDzAK.rb
+++ players/sACHINaNDzAK.rb
@@ -1,23 +1,19 @@
-class HumanPlayer
+class StupidPlayer
   def name
-    "Human Player"
+    "Sachin and Zak Player"
   end
 
   def new_game
-    [3, 4, 5, 3, 2].map{ |length|
-      puts "ship of length #{length}"
-      puts "co-ordinates of top left (x,y)?"
-      x, y = $stdin.gets.split(",").map{ |a| a.strip.to_i }
-      puts "orientation (\"a\" for across or \"d\" for down)?"
-      orientation = $stdin.gets.strip == "a" ? :across : :down
-      [x, y, length, orientation]
-    }
+    [
+        [1, 1, 5, :across],
+        [2, 2, 4, :across],
+        [3, 3, 3, :across],
+        [7, 6, 3, :across],
+        [1, 9, 2, :across]
+    ]
   end
 
   def take_turn(state, ships_remaining)
-    # puts "ships remaining: #{ships_remaining.inspect}"
-    # puts "co-ordinates (x,y)?"
-    # x, y = $stdin.gets.split(",").map{ |a| a.strip.to_i }
     [rand(10), rand(10)]
   end
 end
